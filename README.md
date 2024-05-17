500e7528c4bd81f6d2b8c05b741369ae72b00b96
denzel.bautista <denzel.bautista@utec.edu.pe>
2024-05-17 05:30:52 +0000

    arreglando assets

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Flask
+# ChallwaCom
 
 A Flask starter template as per the docs: https://flask.palletsprojects.com/en/3.0.x/quickstart/#a-minimal-application
--- src/main.py
+++ src/main.py
@@ -8,6 +8,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+@app.route("/vender")
+def sell():
+    return render_template('product-details.html')
+
+@app.route("/peces")
+def fishes():
+    return render_template('product-details.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
 def main():
     app.run(port=int(os.environ.get('PORT', 3000)))
 
